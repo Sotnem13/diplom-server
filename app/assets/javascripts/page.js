4b2f1207c56d924b2a81d13e8d58f51c2bcb0d6a
sotnem <lina211090@gmail.com>
2017-03-23 20:54:04 +0300

    fix video modal

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -3,9 +3,16 @@ $(document).ready(function () {
 
 
     $('.image_preview').on('click',function () {
-
-        console.log(this.src);
+        $('#video').hide();
+        $('.modal-content .image').show()
         $('.modal-content .image').css('background-image', 'url(' + this.src +  ')');
+    });
+
+    $('.video_preview').on('click',function () {
+        $('#video')[0].src = this.src;
+        $('#video').show();
+
+        $('.modal-content .image').hide();
     });
 
 });
