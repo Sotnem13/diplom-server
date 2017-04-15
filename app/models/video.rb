5be2e512fcb581e92ccd97f8ae47bf335dc30396
Sotnem <lina211090@gmail.com>
2017-04-15 15:13:16 +0300

    make emoji showable for vidoe

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,4 +9,15 @@ class Video < ActiveRecord::Base
 
   accepts_nested_attributes_for :emotions, allow_destroy: true
 
+  def emojis
+    data = emotions.map(&:name)
+    result = data.map do |e|
+      image = Emoji.image_url_for_unicode_moji(e)
+      asset_path = ActionController::Base.helpers.asset_path(image)
+      ActionController::Base.helpers.image_tag(asset_path)
+    end.join(' ')
+    result.tr!('"', "'")
+    result
+  end
+
 end
